cd98ec6722094e11d338513736f1af450148a0e0
EC2 Default User <ec2-user@ip-172-31-43-17.us-east-2.compute.internal>
2018-10-27 19:05:55 +0000

    All functional, small bug in owl service.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
+var fs = require('fs');
+var request = require('request')
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.sendFile('weather.html', { root: 'public' });
+});
+
+var owl = "https://owlbot.info/api/v1/dictionary/";
+router.get('/owl', function(req,res) {
+    owl = owl + req.query.q;
+  request(owl).pipe(res);
+});
+
+router.get('/getcity', function(req, res, next) {
+    fs.readFile(__dirname + '/cities.dat.txt', function(err, data) {
+        if (err) throw err;
+        var cities = data.toString().split("\n");
+        var myRe = new RegExp("^" + req.query.q);
+        var jsonresult = [];
+        for (var i = 0; i < cities.length; i++) {
+            var result = cities[i].search(myRe);
+            if (result != -1) {
+                jsonresult.push({ city: cities[i] });
+            }
+        }
+        res.status(200).json(jsonresult);
+    });
 });
 
 module.exports = router;
