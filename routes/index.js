518ccc3c6e57a7f2e04075dab0bac48dc60cf378
EC2 Default User <ec2-user@ip-172-31-43-17.us-east-2.compute.internal>
2018-11-02 04:32:50 +0000

    Final

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-        console.log("here");
         res.sendFile('index.html', { root: 'public' });
+});
+
+router.get('/mansion', function(req, res, next) {
+        console.log("here");
+        res.sendFile('mansion.jpg', { root: 'public' });
 });
 
 router.get('/calculate', function(req, res, next) {
@@ -36,12 +40,23 @@ router.get('/calculate', function(req, res, next) {
 router.get('/afford', function(req, res, next) {
         var income = req.query.income;
         var payment = req.query.payment;
+        var credit = req.query.credit;
 
         var percentage = payment / income;
-        console.log(percentage);
-        console.log(payment);
-        console.log(income);
-        var shouldYou = percentage > 0.3 ? "No" : "Yes";
+        var actual = (percentage * 100).toFixed(2);
+        var shouldYou;
+        if (percentage > 0.3) {
+                shouldYou = "You can't afford this house. You want your mortgage to be no more than 30% of your income. Your mortgage would be " + actual + "%."; 
+        }
+        else if (credit < 660 && credit > 580 && percentage < 0.3) {
+                shouldYou = "You can, but you shouldn't. Try to get your credit score above 660 if possible."
+        }
+        else if (credit < 580) {
+                shouldYou = "You make enough, but you can't buy a house. You need to work on your credit. The minimum to buy a house is 580.";
+        }
+        else {
+                shouldYou = "Yes, you can! Go crazy!";
+        }
 
         var jsonresult = [];
         jsonresult.push({ shouldYou: shouldYou });
