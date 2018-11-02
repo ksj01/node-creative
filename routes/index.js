20de8102ed89fb334c46f5345b799365c0e0a52d
EC2 Default User <ec2-user@ip-172-31-43-17.us-east-2.compute.internal>
2018-11-02 03:42:53 +0000

    Main logic complete

--- routes/index.js
+++ routes/index.js
@@ -5,30 +5,48 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.sendFile('weather.html', { root: 'public' });
+        console.log("here");
+        res.sendFile('index.html', { root: 'public' });
 });
 
+router.get('/calculate', function(req, res, next) {
+        var mortgage = req.query.mortgage;
+        var rate = req.query.rate;
+        var time = req.query.time;
+        var down = req.query.down;
+        var credit = req.query.credit;
+        var interest = (mortgage * (rate / 100)) * time;
+        var payoff = parseInt(interest) + parseInt(mortgage);
+        var downpercent = (down / mortgage) * 100;
+        var payment = payoff / (time * 12);
+        
 
-router.get('/owl', function(req,res) {
-    var owl = "https://owlbot.info/api/v1/dictionary/";
-    owl = owl + req.query.q;
-  request(owl).pipe(res);
+        var o = [];
+        
+        var data = {
+                payoff: payoff.toFixed(2),
+                downpercent: downpercent.toFixed(2),
+                payment: payment.toFixed(2)
+        };
+        o.push(data);
+
+        res.status(200).json(o);
 });
 
-router.get('/getcity', function(req, res, next) {
-    fs.readFile(__dirname + '/cities.dat.txt', function(err, data) {
-        if (err) throw err;
-        var cities = data.toString().split("\n");
-        var myRe = new RegExp("^" + req.query.q);
+router.get('/afford', function(req, res, next) {
+        var income = req.query.income;
+        var payment = req.query.payment;
+
+        var percentage = payment / income;
+        console.log(percentage);
+        console.log(payment);
+        console.log(income);
+        var shouldYou = percentage > 0.3 ? "No" : "Yes";
+
         var jsonresult = [];
-        for (var i = 0; i < cities.length; i++) {
-            var result = cities[i].search(myRe);
-            if (result != -1) {
-                jsonresult.push({ city: cities[i] });
-            }
-        }
+        jsonresult.push({ shouldYou: shouldYou });
+
         res.status(200).json(jsonresult);
-    });
 });
 
 module.exports = router;
